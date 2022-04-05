d196118f1505ee3a9ad346763ca4873b55e63b40
Johnny Chen <johnnyc1314@gmail.com>
2022-04-05 12:44:06 -0400

    commit tree 1e85fef2337b05848d9105371cdf3b2f4a0bf01b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(n){
+    bestCustomer = n
+}
+const leastFavoriteCustomer = 'i';
+function changeLeastFavoriteCustomer(n){
+    leastFavoriteCustomer = n
+}
